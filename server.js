3bc83a3b097e635cb541324cfa1f5f8bb65520c1
sumit kumar <sumit1523@gmail.com>
2023-06-01 04:35:27 +0530

    node.js.yml removed test cmd

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); // Parsing Data
 app.use("/api/contacts", require("./routes/contactRoutes"));
-app.use(errorHandler);
+
+app.use(errorHandler); // Error handling middleware
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
